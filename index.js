090bd7e2bd4dc96532f1bc9a97604fd47e5791b5
LoopFruits <Kensworldc@gmail.com>
2022-08-07 10:20:14 -0400

    commit tree 17eacec6456974a7832d25fa2292ed622bee7a4c

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-let num1 = 31;
-let num2 = 2;
+// create a variable called multiply set to an equation that will multiply variable num1 and num2 to equal 62
+let num1 = 62
+let num2 = 1
 
 const multiply = num1 * num2;
 
-const random = Math.floor(Math.random() * 10) + 1;
+// create a variable called random that will generate a random integer greater than 0
+const random = Math.floor(Math.random() * 21)
 
-
-let num3 = 12
+//create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4
+let num3 = 12 
 let num4 = 3
 
-const mod = (num3 / num4)
+const mod = num3 / num4
 
 
-const max = Math.max(1, 2 , 3 , 5 , 20);
+//creater a variable called max that finds the highest number in a set; the value returned should be 20
 
+const max = Math.max(1,2,3,4,20);
